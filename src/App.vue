bc7bcb111f3a019f9b275b6df28583863b338ac8
Bruno <mosciatti.bruno@Gmail.com>
2019-02-07 14:37:42 -0200

    gogogo

--- src/App.vue
+++ src/App.vue
@@ -75,83 +75,29 @@
             />
             <p>4 FUN</p>
           </span>
-          <div class="noPadd sv-card">
+
+          <div class="noPadd sv-card" v-for="server in gc.forfun.servers.slice(0, 3)">
             <ul>
               <li>
-                <a href="" title="Copiar IP"><i class="far fa-copy"></i></a>
+                <!-- rip clipboard -->
+                <a v-bind:href="server.copy" title="Copiar IP"><i class="far fa-copy"></i></a>
               </li>
               <li>
-                <a href="" title="Entrar no servidor"
-                  ><i class="fas fa-sign-in-alt"></i
-                ></a>
+                <a v-bind:href="server.join" title="Entrar no servidor"><i class="fas fa-sign-in-alt"></i></a>
               </li>
             </ul>
-            <span class="sv-name">#133 - Deathmatch</span>
-            <span class="sv-mode">DM FFA</span>
-            <span class="sv-map">de_overpass <em>08/16</em></span>
+            <span class="sv-name">#{{server.id}} - {{server.title}} </span>
+            <span class="sv-mode">{{server.mode}}</span>
+            <span class="sv-map">{{server.map}} <em>{{server.current}}/{{server.max}}</em></span>
             <div class="progress">
-              <div
-                class="progress-bar"
-                role="progressbar"
-                aria-valuenow="50"
-                aria-valuemin="0"
-                aria-valuemax="100"
-                style="width:50%"
-              >
+              <div class="progress-bar" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width:50%">
                 <span class="sr-only">50% ocupado</span>
               </div>
             </div>
           </div>
-          <div class="noPadd sv-card">
-            <ul>
-              <li>
-                <a href="" title="Copiar IP"><i class="far fa-copy"></i></a>
-              </li>
-              <li>
-                <a href="" title="Entrar no servidor"><i class="fas fa-sign-in-alt"></i></a>
-              </li>
-            </ul>
-            <span class="sv-name">#133 - Deathmatch</span>
-            <span class="sv-mode">DM FFA</span>
-            <span class="sv-map">de_overpass <em>08/16</em></span>
-            <div class="progress">
-              <div
-                class="progress-bar"
-                role="progressbar"
-                aria-valuenow="50"
-                aria-valuemin="0"
-                aria-valuemax="100"
-                style="width:50%"
-              >
-                <span class="sr-only">50% ocupado</span>
-              </div>
-            </div>
-          </div>
-          <div class="noPadd sv-card">
-            <ul>
-              <li>
-                <a href="" title="Copiar IP"><i class="far fa-copy"></i></a>
-              </li>
-              <li>
-                <a href="" title="Entrar no servidor"><i class="fas fa-sign-in-alt"></i></a>
-              </li>
-            </ul>
-            <span class="sv-name">#133 - Deathmatch</span>
-            <span class="sv-mode">DM FFA</span>
-            <span class="sv-map">de_overpass <em>08/16</em></span>
-            <div class="progress">
-              <div
-                class="progress-bar"
-                role="progressbar"
-                aria-valuenow="50"
-                aria-valuemin="0"
-                aria-valuemax="100"
-                style="width:50%"
-              >
-                <span class="sr-only">50% ocupado</span>
-              </div>
-            </div>
-          </div>
+
+
+
         </div>
         <div class="col-xs-3 lobby">
           <span
@@ -159,22 +105,21 @@
               title="LOBBY"
               src="/src/assets/images/lobby.svg"
               width="20px"
-            />
-            <p>LOBBY</p></span
-          >
+            /> <!-- ok it could dynamic like pice of cake... <3 but... -->
+            <p>{{gc.games[0].title}}</p></span>
           <div class="card-stats col-md-4">
-            <h3 class="partidas">1564</h3>
+            <h3 class="partidas">{{gc.games[0].matches}}</h3>
             <h4>partidas</h4>
           </div>
           <div class="card-stats col-md-4">
-            <h3 class="vitorias">1100</h3>
+            <h3 class="vitorias">{{gc.games[0].win}}</h3>
             <h4>vitórias</h4>
           </div>
           <div class="card-stats col-md-4">
-            <h3 class="derrotas">464</h3>
+            <h3 class="derrotas">{{gc.games[0].lose}}</h3>
             <h4>derrotas</h4>
           </div>
-          <a href="#" title="Ir para o lobby">IR PARA LOBBY</a>
+          <a v-bind:href="gc.games[0].cta.link" title="Ir para o lobby">{{gc.games[0].cta.title}}</a>
         </div>
 
         <div class="col-xs-3 ranked">
@@ -184,22 +129,21 @@
               src="/src/assets/images/Open.svg"
               width="20px"
             />
-            <p>RANKED OPEN</p></span
+            <p>{{gc.games[1].title}}</p></span
           >
           <div class="card-stats col-md-4">
-            <h3 class="partidasRanked">1564</h3>
+            <h3 class="partidasRanked">{{gc.games[1].matches}}</h3>
             <h4>partidas</h4>
           </div>
           <div class="card-stats col-md-4">
-            <h3 class="vitorias">1100</h3>
+            <h3 class="vitorias">{{gc.games[1].win}}</h3>
             <h4>vitórias</h4>
           </div>
           <div class="card-stats col-md-4">
-            <h3 class="derrotas">464</h3>
+            <h3 class="derrotas">{{gc.games[1].lose}}</h3>
             <h4>derrotas</h4>
           </div>
-          <a href="#" title="Ir para o lobby"
-            ><em><i class="fas fa-users"></i> 15</em> entrar na fila</a>
+          <a v-bind:href="gc.games[1].cta.link" title="Entrar na fila"><em><i class="fas fa-users"></i> 15</em> {{gc.games[1].cta.title}}</a>
         </div>
         <div class="col-xs-12 noPadd footer">
           <a href="#"
@@ -209,7 +153,7 @@
             <p>Jogadores<em>Online</em></p>
             <div class="ban">
               <span>{{ gc.latest_banned }}</span>
-              <p>Cheaters Banidqos<em>Nos Últimos 7 dias</em></p>
+              <p>Cheaters Banidos<em>Nos Últimos 7 dias</em></p>
             </div>
           </div>
         </div>
